4acfd4c188d9700bf046329ad836fbd58712d1ec
Kay McCormick <kay@kaymccormick.com>
2018-12-04 16:37:41 -0800

    Update macro handling

--- python/graph2.py
+++ python/graph2.py
@@ -18,26 +18,76 @@ obj_perm_sets_spt = '/home/user/j/jade/obj_perm_sets.spt'
 output_dir = "/home/user/j/jade/public_html/graphs"
 perm_sets_json = 'perm_sets.json'
 
+class Macro:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new):
+        self._name = new
+
+class PermSet(Macro):
+    pass
+
+classes1 = {}
+classes2 = {}
 with open("classes.txt", "r") as f:
     for a in f:
         a = a.rstrip()
         r = string_to_security_class(a)
-        print("%d %s" % (r, a))
+        classes1[a] = r
+        classes2[r] = a
 
-exit(1)
-        
-i = 0
-errors = 0
-while errors < 5:
-    x = security_class_to_string(i)
-    if x:
-        print("%d %s" % i, x)
-        errors = 0
-    else:
-        errors += 1
-    i = i + 1
-    
-    
+
+macro_perms = {}
+with open(perm_sets_json, 'r') as f:
+    perm_sets = json.load(f)
+    def _():
+        pass
+    perm_sets2 = {}
+    refs = {}
+    for macro_name, ary in perm_sets.items():
+        m_perms = []
+        m_refs = []
+        for perm in ary:
+            if perm in perm_sets:
+                m_refs.append(perm)
+                if perm not in refs:
+                    refs[perm] = []
+                refs[perm].append(macro_name)
+            else:
+                m_perms.append(perm)
+        perm_sets2[macro_name] = (m_perms, m_refs)
+
+    def _resolve(mp, ps, refname):
+        if refname in mp:
+            return mp[refname]
+        (p, r) = ps[refname]
+        perms = []
+        for ref in r:
+            refperms = _resolve(mp, ps, ref)
+            perms.extend(refperms)
+        perms.extend(p)
+        mp[refname] = perms
+        return perms
+            
+    for macro_name in perm_sets.keys():
+        if macro_name not in refs:
+            m_perms = []
+            (p,r) = perm_sets2[macro_name]
+            for ref in r:
+                refperms = _resolve(macro_perms, perm_sets2, ref)
+                m_perms.extend(refperms)
+            m_perms.extend(p)
+            mp = {}
+            for x in m_perms:
+                mp[x] = True
+            macro_perms[macro_name] = list(mp.keys())
+
 def slurp_arg(contents):
     if(contents[0] == '`'):
         match = re.match(r'`([^`\']+)\'', contents)
@@ -87,13 +137,7 @@ def parse_obj_perm_sets(filename):
                         perms = permset.split(' ')
                         my_perms = {}
                         for perm in perms:
-                            print(repr(perm))
-                            if perm in perm_sets:
-                                for perm2 in perm_sets[perm]:
-                                    my_perms[perm2] = True
-
-                            else:
-                                my_perms[perm] = True
+                            my_perms[perm] = True
 
                         perm_sets[arg] = list(my_perms.keys())
                             
@@ -101,13 +145,14 @@ def parse_obj_perm_sets(filename):
     return perm_sets
 
 
-perm_sets = parse_obj_perm_sets(obj_perm_sets_spt)
-json.dump(perm_sets, fp=sys.stdout)
-by_perms = {}
-for k, v in perm_sets.items():
-    # sorting doesn't make sense!
-    by_perms[' '.join(sorted(v))] = k
+#perm_sets = parse_obj_perm_sets(obj_perm_sets_spt)
+#json.dump(perm_sets, fp=sys.stdout, indent=4)
 
+#by_perms = {}
+#for k, v in perm_sets.items():
+#    # sorting doesn't make sense!
+#    by_perms[' '.join(sorted(v))] = k
+#
 
 sh = semanage_handle_create()
 if semanage_connect(sh):
@@ -123,8 +168,10 @@ while i < b:
     if not name.startswith('us_heptet'):
         continue
     print(name)
+
+    format = 'svg'
     
-    dot = Digraph(comment="Graph for module %s" % name, format='svg')
+    dot = Digraph(comment="Graph for module %s" % name, format=format)
 #    dot.node('module %s' % module, 'Module %s' % name)
     
     (result, modkey) = semanage_module_key_create(sh)
@@ -155,8 +202,15 @@ while i < b:
             perm_str = ''
             permvals = []
             avs = 0
+            perms2 = []
             for p in perms:
                 perm = p.value()
+                if perm in macro_perms:
+                    perms2.extend(macro_perms[perm])
+                else:
+                    perms2.append(perm)
+
+            for perm in perms2:
                 perm_av = string_to_av_perm(security_class, perm)
                 avs = avs | perm_av
                 print("%16s %08x" % (perm, perm_av))
@@ -166,16 +220,10 @@ while i < b:
             if not result:
                 print("AV string is %s" % av_string)
             
-
             permvals = sorted(permvals)
             perm_str = ' '.join(permvals)
             #perm_str += ' }'
             perm_str = perm_str.lstrip()
-            perm_macro = ''
-            if perm_str in by_perms:
-                perm_macro = by_perms[perm_str]
-                perm_str = perm_macro
-                print("YAY", perm_macro)
                 
             dot.edge('type %s' % entry[1].value(), 'type %s' % entry[2].value(),
                      class_symbol.value() + ' ' + perm_str)
